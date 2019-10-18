41bc6caa10f2c602acac0ee37cbc6f1728ad5f7d
karen-ifrt <karen-ifrt@users.noreply.github.com>
2019-10-18 16:00:54 +0200

    First version

--- script.js
+++ script.js
@@ -1,18 +1,17 @@
+
 mybutton = document.getElementById("myBtn");
 
-function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-};
-
 window.addEventListener("scroll", function(){
-  scrollFunction();
+  if (document.documentElement.scrollTop > 200) {
+    mybutton.style.bottom = "30px";
+
+  } else {
+    mybutton.style.bottom = "-100px";
+  }
 });
 
 function topFunction() {
     $("html").animate({ scrollTop: "0" }, "600");
 }
+
 
